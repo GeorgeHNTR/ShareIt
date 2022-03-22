79964a70f98eec391e79f3387c959f7ef7a32a5f
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-03-22 20:51:53 +0200

    add storage of  all wallets a specific user is a member of

--- contracts/SharedWalletFactory.sol
+++ contracts/SharedWalletFactory.sol
@@ -4,17 +4,25 @@ pragma solidity ^0.8.0;
 import "./SharedWallet.sol";
 
 contract SharedWalletFactory {
+    // All wallets ever created
     address[] public wallets;
 
+    // All wallets a specific user is a member of
+    mapping(address => address[]) public userWallets;
+
+    // Creates a new wallet instance
     function createNewSharedWallet(uint256 _maxMembers) external {
         SharedWallet wallet = new SharedWallet(_maxMembers);
-        _addNewSharedWallet(address(wallet));
+        _addNewSharedWallet(msg.sender, address(wallet));
     }
 
-    function _addNewSharedWallet(address _walletAddress) private {
+    // Stores a newly created wallet in both "wallets" address array and "userWallets" mapping
+    function _addNewSharedWallet(address user, address _walletAddress) private {
         wallets.push(_walletAddress);
+        userWallets[user].push(_walletAddress);
     }
 
+    // Getter function that returns "wallets" address array (all wallets ever created)
     function getWallets() public view returns (address[] memory) {
         return wallets;
     }
