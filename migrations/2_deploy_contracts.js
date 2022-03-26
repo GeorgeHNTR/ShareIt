f3b2ad60ac1db9c437357ab633b9f0076318529c
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-03-26 12:06:17 +0200

    deploy only factory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
 const SharedWalletFactory = artifacts.require("SharedWalletFactory");
 
-module.exports = async function (deployer, network, accounts) {
+module.exports = async function (deployer) {
     await deployer.deploy(SharedWalletFactory);
-    if(network == 'development') {
-        const factory = await SharedWalletFactory.deployed();
-        await factory.createNewSharedWallet();
-    }
 };
